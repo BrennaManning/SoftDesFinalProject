a302fed9bc1064e756ee8ffead799ef0691df5d9
Brenna Manning <brenna.manning@students.olin.edu>
2015-04-14 22:57:08 -0400

    frequency calculator

--- pygame_breadboard.py
+++ pygame_breadboard.py
@@ -36,24 +36,27 @@ class Background(pygame.sprite.Sprite):
 class Nodes():
     """ represents all indicated places for circuit connectors """
     def __init__(self, pos_x, pos_y):
-    	pass
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
 
     def update(self):
         """ updates current and voltage values of nodes"""
         pygame.event.pump()
 
-# 	def calculate_voltage(self,model):
+#   def calculate_voltage(self,model):
 
-# 	def calculate_current(self,model):
+#   def calculate_current(self,model):
 
 class Connections():
-	""" represents all placed connections, including wires, resistors, and 
-		capacitors """
-	def __init___(self, node_1, node_2, type, value):
-		pass
+    """ represents all placed connections, including wires, resistors, and 
+        capacitors """
+    def __init___(self, node_1, node_2, type, value):
+        pass
 
-	def get_drawables():
-		""" draws the circuit components on the breadboard """
+    def get_drawables():
+        """ draws the circuit components on the breadboard """
+
 
 class Model():
     """ Represents the game state of the scroller """
@@ -70,18 +73,46 @@ class Model():
         events = pygame.event.get()
 
     def end_program(self):
-    	"""ends the program"""
-    	pass
+        """ends the program"""
+        pass
 
     def get_background_drawables(self):
         """ Return a list of DrawableSurfaces for the model """
         return self.background.get_drawables()
 
     def add_node(self,mpos):
-    	""" adds a node to the list of nodes, to be used in voltage and 
-    		current calculations"""
-    	self.nodes.append(Nodes(mpos[0],mpos[1]))
-    	print len(self.nodes)
+
+        """ adds a node to the list of nodes, to be used in voltage and 
+            current calculations"""
+        self.nodes.append(Nodes(mpos[0],mpos[1]))
+        print len(self.nodes)
+
+
+    def calculate_cutoff_frequency(self, blocks):
+        if level == 1: #Different setup & equations for different levels
+            """CALCULATE CUTOFF FREQUENCY FOR LEVEL ONE - PASSIVE LOW PASS FILTER"""
+            # if blocks is a list of block lists and each block list contains [ 'r/c', 'value#']
+            """for example:"""
+            blocks = [['r', 100], ['c', 1]]
+            #level one has two blocks because it needs 1 resistor and 1 capacitor to build the passive low pass filter"""
+            block1 = blocks[1]
+            block2 = blocks[2]
+            fail = False
+            if block1[1] == 'r':
+                r1 = block1[2]
+            else:
+                fail = True
+            if block2[1] == 'c':
+                c1 = block2[2]
+            else:
+                fail = True
+            if fail == True:
+                print "Incorrect setup for passive low-pass filter. Please try again."
+            else:
+                cutoff_frequency = 1/(2*3.14*r1*c1)
+
+                print "cutoff_frequency = " + cutoff_frequency
+
 
 class View():
     def __init__(self, g_model, width, height):
@@ -103,31 +134,32 @@ class View():
         self.screen.blit(surf, rect)
 
 class Controller():
-	def __init__(self, model):
-		self.model = model
-		self.mouse_pressed = False
+    def __init__(self, model):
+        self.model = model
+        self.mouse_pressed = False
 
-	def process_events(self):
-		""" process keyboard events. Function called periodically """
-		pass
+    def process_events(self):
+        """ process keyboard events. Function called periodically """
+        pass
 
-	def add_connection(self):
-		""" processes the series of clicks required to generate connections
-			outputs a list consisting of the first connection node, the second,
-			the connection type (wire, R,C, etc), and the connection value """
-		connection_factors = []
+    def add_connection(self):
+        """ processes the series of clicks required to generate connections
+            outputs a list consisting of the first connection node, the second,
+            the connection type (wire, R,C, etc), and the connection value """
+        connection_factors = []
 
-		pygame.event.pump()
-		if pygame.mouse.get_pressed() != (1, 0, 0):
-			self.mouse_pressed = False
-		elif not (self.mouse_pressed):
-			self.mouse_pressed = True
-			mpos = pygame.mouse.get_pos()
-			if len(connection_factors) < 2:
-				mpos = ((mpos[0]-275)/70 + 1,(mpos[1]-65)/85 + 1)
-				if mpos[0] > 0 and mpos[0] < 7:
-					if mpos[1] > 0 and mpos[1] < 4:
-						self.model.add_node(mpos)
+        pygame.event.pump()
+        if pygame.mouse.get_pressed() != (1, 0, 0):
+            self.mouse_pressed = False
+        elif not (self.mouse_pressed):
+            self.mouse_pressed = True
+            mpos = pygame.mouse.get_pos()
+            if len(connection_factors) < 2:
+                mpos = ((mpos[0]-275)/70 + 1,(mpos[1]-65)/85 + 1)
+                if mpos[0] > 0 and mpos[0] < 7:
+                    if mpos[1] > 0 and mpos[1] < 4:
+                        self.model.add_node(mpos)
+
 
 class pygameBreadboard():
     """ The main class """
