c4127b1e18fed13f2ef667718a75f543566dd5c5
Brenna Manning <brenna.manning@students.olin.edu>
2015-04-22 23:54:11 -0400

    restructuring update into functions and making component list

--- pygame_breadboard/model.py
+++ pygame_breadboard/model.py
@@ -12,9 +12,8 @@ class Model():
         self.width = width
         self.height = height
         self.background = Background()
-        self.pos = ()
-        self.components = []
-        
+ #       self.pos = ()
+#        self.components = []
         self.state = 0
         self.states = ["State 0: user needs to click the component to begin placement", "State 1: user needs to click the position of the component"]
 
@@ -56,7 +55,7 @@ class Model():
     #                     component_value = self.define_value(self.value)
     #                     self.spot = spot
     #                     DrawComponent(spot, component_type)
-
+ 
 
     def update_stuff(self):
         """ updates all aspects of the game """
@@ -65,89 +64,39 @@ class Model():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mpos = pygame.mouse.get_pos() 
                 if self.state == 0:
-                    self.selected = "none"
+                    self.mpos1 = mpos
+                    self.mpos2 = (0,0)
+                    Component.define_type(Component(self.mpos1, self.mpos2),mpos)
+                    Component.define_value(Component(self.mpos1, self.mpos2),mpos)
                     if mpos[0] > 60 and mpos[0] < 180 and mpos[1] > 180 and mpos [1] < 360:
-                        if mpos[1] < 225 and mpos[1] > 180:
-                            self.selected = "r1"
-                            self.value = "r1 value"
-                        elif mpos[1] < 270 and mpos[1] >225:
-                            self.selected = "r2"
-                            self.value = "r2 value"
-                        elif mpos[1] < 315 and mpos[1] > 270:
-                            self.selected = "c1"
-                            self.value = "c1 value"
-                        elif mpos[1] < 360 and mpos[1] > 315:
-                            self.selected = "c2"
-                            self.value = "c2 value"
-                        #print self.selected
-                        
-                        component_type = self.define_type(self.selected)
-                        
-                        component_value = self.define_value(self.value)
-
-                        self.getspotcomponent(component_type, component_value)
-                        
+                        self.state = 1
+                    print(self.states[self.state])
+                    
+                  
                 else: #self.state == 1
                     #print self.selected
+                    print(self.states[self.state])
+
                     if mpos[0] > 200:
                         if self.state == 1:
-                            self.pos = mpos
-                            self.define_spot(self.pos)
-                            spot = self.define_spot(self.pos)
-                            self.get_components
+                            self.mpos2 = mpos
+                            Component(self.mpos1,self.mpos2).define_spot(mpos)
+                            self.model_get_components
+                            print(self.states[self.state])
                             self.state = 0 
+
+                           
 
                 print(self.states[self.state])
 
-    def getspotcomponent(self, ctype, value):
-        self.state = 1
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                mpos = pygame.mouse.get_pos() 
-
-                if mpos[0] > 200:
-                    if self.state == 1:
-                        self.pos = mpos
-                        self.define_spot(self.pos)
-                        spot = self.define_spot(self.pos)
-                        self.get_components
-                        self.component_type = ctype
-                        self.component_value = value
-                        print "DRAW!!"
-                        DrawComponent(spot, component_type, component_value)
-                        self.state = 0
+    # def getspotcomponent(self, ctype, value):
+    #    #DO NOT USE
+    #             print "DRAW!!"
+    #             DrawComponent(spot, component_type, component_value)
+    #             self.state = 0
 
 
-    def define_spot(self,mpos):
-        """ determines which specific spot had been clicked """
-        mpos_coord = ((mpos[0] - 217)/95, (mpos[1] - 127)/95)
-       # print mpos_coord
-        if mpos_coord == (1,0) or mpos_coord == (2,0):
-            spot = "1"
-            print spot
-            return spot
-        if mpos_coord == (3,1) or mpos_coord == (3,2):
-            spot = "2"           
-            print spot
-            return spot
-
-    def define_type(self, selected):
-        """determines what type of component is selected"""
-        if selected == 'r1' or selected == 'r2':
-            component_type = "R" 
-        if selected == 'c1' or selected == 'c2':
-            component_type = "C" 
-        print component_type
-        return component_type
-
-    def define_value(self, value):
-        """determines the value of the resistor or capacitor - returns a number"""
-        component_value = value 
-
-        print component_value
-        return component_value
-
+    
     
     def end_program(self):
     	"""ends the program"""
@@ -159,20 +108,12 @@ class Model():
         """ Return a list of DrawableSurfaces for the model """
         return self.background.get_drawables()
 
-    def get_components(self):
-        """forms list of components"""
-
-        #This function is not currently being called
-        ######
-        components = self.components
-        spot = self.define_spot
-        print spot + "get_components function"
-        component_type = self.define_type
-        print component_type + "get_components function"
-        value = self.define_value
-        print value + "get_components function"
-        component = [component_type, spot, value]
-        components.append(component)
+    def model_get_components(self,mpos1,mpos2):
+        """forms list of components in model class"""
+        components = []
+        component = Component(mpos1,mpos2).get_component
+        if component != [None, None, None]:
+            components.append(component)
        
         #print components
 
@@ -197,20 +138,91 @@ class Background(object):
         """Gets the drawables for the background"""
         return DrawableSurface(self.image,pygame.Rect((0,0), self.image.get_size()))
 
-
-class component():
-    def __init__(self, pos, type, value):
+class Component():
+    def __init__(self, mpos1, mpos2):
         """initializes a component"""
-        self.pos = pos
-        self.type = type
-        if self.type == r:
+        self.component_type = self.define_type(mpos1)
+        self.component_value = self.define_value(mpos1)
+        self.mpos2 = mpos2
+        self.spot = self.define_spot(self.mpos2)
+
+        if self.component_type == 'R':
             self.image = pygame.image.load('images/Resistor.png')
             self.image = pygame.transform.scale(self.image, (120, 30))
-        if self.type == c:
+        if self.component_type == 'C':
             self.image = pygame.image.load('images/Capacitor.png')
             self.image = pygame.transform.scale(self.image, (120, 30))
 
-        print self.pos
+
+        self.component = self.get_component()
+
+    def define_type(self, mpos):
+        """determines what type of component is selected"""
+
+        self.selected = "none"
+        if mpos[0] > 60 and mpos[0] < 180 and mpos[1] > 180 and mpos [1] < 360:
+            if mpos[1] < 225 and mpos[1] > 180:
+                self.selected = "r1"
+                self.value = "r1 value"
+            elif mpos[1] < 270 and mpos[1] >225:
+                self.selected = "r2"
+                self.value = "r2 value"
+            elif mpos[1] < 315 and mpos[1] > 270:
+                self.selected = "c1"
+                self.value = "c1 value"
+            elif mpos[1] < 360 and mpos[1] > 315:
+                self.selected = "c2"
+                self.value = "c2 value"
+            
+            if self.selected == 'r1' or self.selected == 'r2':
+                component_type = "R" 
+            if self.selected == 'c1' or self.selected == 'c2':
+                component_type = "C" 
+            print component_type
+            
+            return component_type
+
+    def define_value(self, mpos):
+        """determines what type of component is selected"""
+        
+       
+        if mpos[0] > 60 and mpos[0] < 180 and mpos[1] > 180 and mpos [1] < 360:
+            if mpos[1] < 225 and mpos[1] > 180:
+                self.value = "r1 value"
+            elif mpos[1] < 270 and mpos[1] >225:
+                self.value = "r2 value"
+            elif mpos[1] < 315 and mpos[1] > 270:
+                self.value = "c1 value"
+            elif mpos[1] < 360 and mpos[1] > 315:
+                self.value = "c2 value"
+            
+            component_value = self.value
+            print component_value
+            model.state = 1
+            return component_value
+            
+       
+
+    def define_spot(self,mpos):
+        """ determines which specific spot had been clicked """
+        mpos_coord = ((mpos[0] - 217)/95, (mpos[1] - 127)/95)
+       # print mpos_coord
+        if mpos_coord == (1,0) or mpos_coord == (2,0):
+            spot = "1"
+            print spot
+            return spot
+        if mpos_coord == (3,1) or mpos_coord == (3,2):
+            spot = "2"           
+            print spot
+            return spot
+
+
+    def get_component(self):
+        """ makes a list to represent the component"""
+        component = []
+        component = [self.component_type, self.component_value, self.spot]
+        print component
+        return component 
 
     def draw_block(self):
         """gets the drawables for the component block"""
